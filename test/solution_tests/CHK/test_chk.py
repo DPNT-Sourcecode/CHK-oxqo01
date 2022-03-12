8a77b20e45bca7131471750d727f16fcaa8d895d
sbx_user1051 <sbx_user1051@169.254.9.21>
2022-03-12 16:48:46 +0000

    Sat Mar 12 16:48:46 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,11 +3,12 @@ from solutions.CHK import checkout_solution
 
 class TestCHK():
     def test_checkout(self):
-        assert checkout_solution.checkout("FF") == 20
+        assert checkout_solution.checkout("FFFF") == 30
 
 
 """
 ("AAACDBBBEEFFF") == 310
 """
+
 
 
