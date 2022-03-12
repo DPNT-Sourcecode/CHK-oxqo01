d51646078d2007e0d75e02c27486a78a1062cc3f
sbx_user1051 <sbx_user1051@169.254.9.21>
2022-03-12 15:14:25 +0000

    Sat Mar 12 15:14:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,32 @@
+ITEMS = {"A": 50, "B": 30, "C": 20, "D": 15}
+OFFERS = {"A": (3, 130), "B": (2, 45)}
 
 
-# noinspection PyUnusedLocal
-# skus = unicode string
+def get_items_map(skus):
+    items_map = {}
+    for item in skus:
+        if item in items_map:
+            items_map[item] += 1
+        else:
+            items_map[item] = 1
+    return items_map
+
+
 def checkout(skus):
-    return 225
+    items_map = get_items_map(skus)
+    checkout_sum = 0
+
+    for k, v in items_map.items():
+        if k in OFFERS and v >= OFFERS[k][0]:
+            offer_number = v // OFFERS[k][0]
+            checkout_sum += (OFFERS[k][1] * offer_number)
+            items_map[k] -= OFFERS[k][0] * offer_number
+
+    for k, v in items_map.items():
+        if k in ITEMS and v > 0:
+            checkout_sum += ITEMS[k]
+
+    return checkout_sum
+
 
 
