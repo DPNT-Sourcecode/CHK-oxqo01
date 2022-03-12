c4d2d2e8c57832f330d9a5664873f5019c628de7
sbx_user1051 <sbx_user1051@169.254.9.21>
2022-03-12 17:03:39 +0000

    Sat Mar 12 17:03:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,49 @@
-ITEMS = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+ITEMS = {"A": 50,
+         "B": 30,
+         "C": 20,
+         "D": 15,
+         "E": 40,
+         "F": 10,
+         "G": 20,
+         "H": 10,
+         "I": 35,
+         "J": 60,
+         "K": 80,
+         "L": 90,
+         "M": 15,
+         "N": 40,
+         "O": 10,
+         "P": 50,
+         "Q": 30,
+         "R": 50,
+         "S": 30,
+         "T": 20,
+         "U": 40,
+         "V": 50,
+         "W": 20,
+         "X": 90,
+         "Y": 10,
+         "Z": 50
+         }
 
-OFFERS = {"discounted_offers": {"A": [(5, 200), (3, 130)], "B": [(2, 45)]},
-          "take_free_offers": {"E": [(2, "B")], "F": [(2, "F")]}}
+OFFERS = {
+    "discounted_offers": {
+        "A": [(5, 200), (3, 130)],
+        "B": [(2, 45)],
+        "H": [(10, 80), (5, 45)],
+        "K": [(2, 150)],
+        "P": [(5, 200)],
+        "Q": [(3, 80)],
+        "V": [(3, 130), (2, 90)]
+},
+    "take_free_offers": {
+        "E": [(2, "B")],
+        "F": [(2, "F")],
+        "N": [(3, "M")],
+        "R": [(3, "Q")],
+        "U": [(3, "U")]
+    }
+}
 
 
 def has_offer(offers, k, v):
@@ -76,3 +118,4 @@ def checkout(skus):
     checkout_sum = discounted_offers(items_map, OFFERS["discounted_offers"])
 
     return checkout_sum
+
